e47068542c67f7e46435a27c692bdea64faa2b9a
Sergey Nevmerzhitsky <sergey.nevmerzhitsky@gmail.com>
2021-11-22 05:12:32 +0300

    Ask user command prototype

--- src/main.rs
+++ src/main.rs
@@ -1,2 +1,47 @@
+use std::io;
+
+trait UserAction {
+    fn execute(&self);
+}
+
+struct MoveNorth {}
+
+struct MoveSouth {}
+
+impl UserAction for MoveNorth {
+    fn execute(&self) {
+        println!("Moving to the north...",);
+    }
+}
+
+impl UserAction for MoveSouth {
+    fn execute(&self) {
+        println!("Moving to the south...");
+    }
+}
+
 fn main() {
+    loop {
+        // Process events
+
+        // Ask user an action
+        let command = ask_command();
+        command.execute();
+    }
+}
+
+fn ask_command() -> impl UserAction {
+    println!("Your action? ");
+
+    let mut input = String::new();
+
+    io::stdin()
+        .read_line(&mut input)
+        .expect("Failed to read input");
+
+    println!("You enter: {}", input);
+
+    // TODO Use https://docs.rs/crate/clap/ as mapper of all available command and as input parser
+    // TODO Return Command instance: input + action instance
+    MoveNorth {}
 }
