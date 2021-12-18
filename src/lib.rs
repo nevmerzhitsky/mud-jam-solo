17fec87581c9fb00a8734ca6c541ff423bdb964f
Sergey Nevmerzhitsky <sergey.nevmerzhitsky@gmail.com>
2021-12-18 23:18:53 +0300

    Convert string command to an action object

--- src/lib.rs
+++ src/lib.rs
@@ -50,7 +50,7 @@ pub mod action {
         }
     }
 
-    pub fn ask_command() -> impl UserAction {
+    pub fn ask_command() -> Box<dyn UserAction> {
         println!("Your action? ");
 
         let mut input = String::new();
@@ -59,10 +59,26 @@ pub mod action {
             .read_line(&mut input)
             .expect("Failed to read input");
 
-        println!("You enter: {}", input);
+        command_to_action(input)
+    }
+
+    fn command_to_action(input: String) -> Box<dyn UserAction> {
+        let mut words = input.split_whitespace();
+        let command = words
+            .next()
+            .unwrap_or("")
+            .to_ascii_lowercase();
+        let params: Vec<&str> = words.collect();
+
+        println!(">>> Command: \"{}\" {:?}", command, params);
 
         // TODO Use clap lib as the input parser to all available commands
         // TODO Return Command instance: input + action instance
-        MoveNorth {}
+        match command.as_str() {
+            "quit" => panic!("You decided to quit"),
+            "north" => Box::new(MoveNorth {}),
+            "south" => Box::new(MoveSouth {}),
+            _ => panic!("Unknown command: {}", command),
+        }
     }
 }
