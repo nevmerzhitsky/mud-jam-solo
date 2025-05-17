f3d7a75eb7faaa2e22406101481c41ac840d1e32
Sergey Nevmerzhitsky <sergey.nevmerzhitsky@gmail.com>
2025-05-18 02:11:56 +0500

    Wrap room exists into enum

--- src/area.rs
+++ src/area.rs
@@ -1,8 +1,7 @@
 use std::cell::RefCell;
 use std::fmt;
+use std::ops::Deref;
 use std::rc::{Rc, Weak};
-
-type RoomConnection = RefCell<Weak<Room>>;
 
 pub trait Entity {
     fn get_id(&self) -> u32;
@@ -25,15 +24,13 @@ impl World {
         let room1 = Rc::new(Room::new_in_void(1));
         let room2 = Rc::new(Room::new_in_void(2));
 
-        *room1.north_room.borrow_mut() = Rc::downgrade(&room2);
-        *room1.south_room.borrow_mut() = Rc::downgrade(&room2);
-        *room1.west_room.borrow_mut() = Rc::downgrade(&room1);
-        *room1.east_room.borrow_mut() = Rc::downgrade(&room1);
+        *room1.north_exit.borrow_mut() = RoomExit::Pathway(Rc::downgrade(&room2));
+        *room1.south_exit.borrow_mut() = RoomExit::Pathway(Rc::downgrade(&room2));
+        *room1.east_exit.borrow_mut() = RoomExit::Pathway(Rc::downgrade(&room1));
 
-        *room2.north_room.borrow_mut() = Rc::downgrade(&room1);
-        *room2.south_room.borrow_mut() = Rc::downgrade(&room1);
-        *room2.west_room.borrow_mut() = Rc::downgrade(&room2);
-        *room2.east_room.borrow_mut() = Rc::downgrade(&room2);
+        *room2.north_exit.borrow_mut() = RoomExit::Pathway(Rc::downgrade(&room1));
+        *room2.south_exit.borrow_mut() = RoomExit::Pathway(Rc::downgrade(&room1));
+        *room2.west_exit.borrow_mut() = RoomExit::Pathway(Rc::downgrade(&room2));
 
         self.area.push(room1);
         self.area.push(room2);
@@ -46,27 +43,32 @@ impl Entity for World {
     }
 }
 
+pub enum RoomExit {
+    DeadEnd,
+    Pathway(Weak<Room>),
+}
+
 pub struct Room {
     id: u32,
     // number: u32,
-    north_room: RoomConnection,
-    south_room: RoomConnection,
-    west_room: RoomConnection,
-    east_room: RoomConnection,
-    up_room: RoomConnection,
-    down_room: RoomConnection,
+    north_exit: RefCell<RoomExit>,
+    south_exit: RefCell<RoomExit>,
+    west_exit: RefCell<RoomExit>,
+    east_exit: RefCell<RoomExit>,
+    up_exit: RefCell<RoomExit>,
+    down_exit: RefCell<RoomExit>,
 }
 
 impl Room {
     pub fn new_in_void(id: u32) -> Room {
         Room {
             id,
-            north_room: RefCell::new(Weak::new()),
-            south_room: RefCell::new(Weak::new()),
-            east_room: RefCell::new(Weak::new()),
-            west_room: RefCell::new(Weak::new()),
-            up_room: RefCell::new(Weak::new()),
-            down_room: RefCell::new(Weak::new()),
+            north_exit: RefCell::new(RoomExit::DeadEnd),
+            south_exit: RefCell::new(RoomExit::DeadEnd),
+            east_exit: RefCell::new(RoomExit::DeadEnd),
+            west_exit: RefCell::new(RoomExit::DeadEnd),
+            up_exit: RefCell::new(RoomExit::DeadEnd),
+            down_exit: RefCell::new(RoomExit::DeadEnd),
         }
     }
 }
@@ -79,10 +81,13 @@ impl Entity for Room {
 
 impl fmt::Debug for Room {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        fn get_connection_caption(rc: &RoomConnection) -> String {
-            match rc.borrow().upgrade() {
-                None => String::from("X"),
-                Some(r) => r.id.to_string(),
+        fn get_exit_caption(rc: &RefCell<RoomExit>) -> String {
+            match rc.borrow().deref() {
+                RoomExit::DeadEnd => String::from("#"),
+                RoomExit::Pathway(wr) => match wr.upgrade() {
+                    None => panic!("No reference on a room exit"),
+                    Some(r) => r.id.to_string(),
+                },
             }
         }
 
@@ -90,12 +95,12 @@ impl fmt::Debug for Room {
             f,
             "Room {{ id: {}, exists (NSWEUD): {}-{}-{}-{}-{}-{} }}",
             self.id,
-            get_connection_caption(&self.north_room),
-            get_connection_caption(&self.south_room),
-            get_connection_caption(&self.west_room),
-            get_connection_caption(&self.east_room),
-            get_connection_caption(&self.up_room),
-            get_connection_caption(&self.down_room)
+            get_exit_caption(&self.north_exit),
+            get_exit_caption(&self.south_exit),
+            get_exit_caption(&self.west_exit),
+            get_exit_caption(&self.east_exit),
+            get_exit_caption(&self.up_exit),
+            get_exit_caption(&self.down_exit)
         )
     }
 }
