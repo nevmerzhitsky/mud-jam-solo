8fc8d8e9718e0c6016919d5087a5a7e0674d8a94
Sergey Nevmerzhitsky <sergey.nevmerzhitsky@gmail.com>
2025-05-17 22:35:34 +0500

    Add enum implementation of commands

--- src/action.rs
+++ src/action.rs
@@ -2,6 +2,10 @@ use std::io;
 
 use crate::area::World;
 
+// ----------------------------------------------------------------------------------------------------
+// Commands via traits
+// ----------------------------------------------------------------------------------------------------
+
 pub trait CharAction {
     fn execute(&self, w: &World, subject_id: u32);
 }
@@ -57,7 +61,7 @@ pub fn ask_command_as_action() -> Box<dyn CharAction> {
     println!("Your action? ");
 
     let mut input = String::with_capacity(200);
-    input.clear();
+    // input.clear();
 
     match io::stdin().read_line(&mut input) {
         Ok(n) => {
@@ -84,5 +88,67 @@ fn command_to_action(input: String) -> Box<dyn CharAction> {
         "south" => Box::new(MoveSouth {}),
         "say" => Box::new(Say { params }),
         _ => Box::new(UnknownCommand {}),
+    }
+}
+
+// ----------------------------------------------------------------------------------------------------
+// Commands via enum
+// ----------------------------------------------------------------------------------------------------
+
+pub enum Command {
+    UnknownCommand,
+    Empty,
+    Quit,
+    MoveNorth,
+    MoveSouth,
+    Say(Vec<String>),
+}
+
+impl Command {
+    pub fn execute(&self, _w: &World, _subject_id: u32) {
+        print!("command: ");
+        match self {
+            Command::UnknownCommand => println!("u"),
+            Command::Empty => {}
+            Command::Quit => println!("q"),
+            Command::MoveNorth => println!("mn"),
+            Command::MoveSouth => println!("ms"),
+            Command::Say(params) => println!("s: {:?}", params),
+        }
+    }
+}
+
+pub fn ask_command_as_enum() -> Command {
+    println!("Your action? ");
+
+    let mut input = String::with_capacity(200);
+    // input.clear();
+
+    match io::stdin().read_line(&mut input) {
+        Ok(n) => {
+            println!("> {n} bytes read: {input}");
+        }
+        Err(error) => println!("error: {error}"),
+    }
+
+    command_to_enum(input)
+}
+
+fn command_to_enum(input: String) -> Command {
+    let mut words = input.split_whitespace();
+    let command = words.next().unwrap_or("").to_ascii_lowercase();
+    let params: Vec<String> = words.map(|m| m.to_string()).collect();
+
+    println!(">>> Command: \"{}\" {:?}", command, params);
+
+    // TODO Use clap library as the input parser to all available commands
+    // TODO Return Command instance: input + action instance
+    match command.as_str() {
+        "" => Command::Empty,
+        "quit" => Command::Quit,
+        "north" => Command::MoveNorth,
+        "south" => Command::MoveSouth,
+        "say" => Command::Say(params),
+        _ => Command::UnknownCommand,
     }
 }
